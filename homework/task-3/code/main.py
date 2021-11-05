2d4d5091287abf5015aedd0e2b8889345090ec4a
Andrew <andrewmosk@icloud.com>
2021-11-05 15:32:10 +0300

    Ввод данных

---
+++ homework/task-3/code/main.py
@@ -0,0 +1,40 @@
+import sys
+from pathlib import Path
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 1 or args[1] not in ['-n', '-f']:
+        print("Usage:\n"
+              "    python3 main.py -n <integer::count of elements> <str::output file> <str::output sorted file>\n"
+              "or\n"
+              "    python3 main.py -f <str::input file> <str::output file> <str::output sorted file>\n")
+    elif args[1] == '-n':
+        if len(args) == 5:
+            cnt = args[2]
+            out_file = args[3]
+            out_sorted_file = args[4]
+            if not cnt.isdigit():
+                print("Count of elements (-n value) must be integer")
+            else:
+                # рандомно
+                pass
+        else:
+            print("In random generator mode, use\n"
+                  "    python3 main.py -n <integer::count of elements> <str::output file> <str::output sorted file>\n"
+                  "Example:\n"
+                  "    python3 main.py -n 1000 outrnd_1.txt outrnd_2.txt")
+    elif args[1] == '-f':
+        if len(args) == 5:
+            in_file = args[2]
+            out_file = args[3]
+            out_sorted_file = args[4]
+            if Path(in_file).is_file():
+                # из файла
+                pass
+            else:
+                print("Input file does not exist")
+        else:
+            print("In file input mode, use\n"
+                  "    python3 main.py -f <str::input file> <str::output file> <str::output sorted file>\n"
+                  "Example:\n"
+                  "    python3 main.py -f in.txt out_1.txt out_2.txt")
